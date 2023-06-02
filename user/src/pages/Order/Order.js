487867f829585a009b71c69e0f946675a1cf67f4
AnhTuandgtl <tuandgtl7@gmail.com>
2023-06-02 20:10:09 +0700

    xóa env

--- user/src/pages/Order/Order.js
+++ user/src/pages/Order/Order.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { React, useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import moment from 'moment';
@@ -28,8 +28,14 @@ import { ORDER_PAY_RESET } from '~/redux/Constants/OrderConstants';
 import { listCart } from '~/redux/Actions/cartActions';
 import Loading from '~/components/HomeComponent/LoadingError/Loading';
 import Message from '~/components/HomeComponent/LoadingError/Error';
-
 import './Order.css';
+// ================
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepButton from '@mui/material/StepButton';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 function Order() {
     const [api, contextHolder] = notification.useNotification();
@@ -139,6 +145,47 @@ function Order() {
             }
         }
     };
+
+    // ==========================================
+    const steps = ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+    const [activeStep, setActiveStep] = useState(0);
+    const [completed, setCompleted] = useState({});
+
+    const totalSteps = () => {
+        return steps.length;
+    };
+
+    const completedSteps = () => {
+        return Object.keys(completed).length;
+    };
+
+    const isLastStep = () => {
+        return activeStep === totalSteps() - 1;
+    };
+
+    const allStepsCompleted = () => {
+        return completedSteps() === totalSteps();
+    };
+
+    const handleNext = () => {
+        const newActiveStep =
+            isLastStep() && !allStepsCompleted()
+                ? // It's the last step, but not all steps have been completed,
+                  // find the first step that has been completed
+                  steps.findIndex((step, i) => !(i in completed))
+                : activeStep + 1;
+        setActiveStep(newActiveStep);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const handleStep = (step) => () => {
+        setActiveStep(step);
+    };
+
+    // ===============================
     return (
         <>
             <div className="container">
@@ -214,7 +261,8 @@ function Order() {
                                 </div>
                             </div>
                         </div>
-                        <div
+                        {/* ============================================== */}
+                        {/* <div
                             className="row order-detail"
                             style={{ border: '1px solid rgb(218, 216, 216)', borderRadius: '4px' }}
                         >
@@ -440,7 +488,38 @@ function Order() {
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
+                        <Box sx={{ width: '100%' }}>
+                            <Stepper nonLinear activeStep={activeStep}>
+                                {steps.map((label, index) => (
+                                    <Step key={label} completed={completed[index]}>
+                                        <StepButton color="inherit" onClick={handleStep(index)}>
+                                            {label}
+                                        </StepButton>
+                                    </Step>
+                                ))}
+                            </Stepper>
+                            <div>
+                                <>
+                                    <Typography sx={{ mt: 2, mb: 1, py: 1 }}>Step {activeStep + 1}</Typography>
+                                    <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                                        <Button
+                                            color="inherit"
+                                            disabled={activeStep === 0}
+                                            onClick={handleBack}
+                                            sx={{ mr: 1 }}
+                                        >
+                                            Back
+                                        </Button>
+                                        <Box sx={{ flex: '1 1 auto' }} />
+                                        <Button onClick={handleNext} sx={{ mr: 1 }}>
+                                            Next
+                                        </Button>
+                                    </Box>
+                                </>
+                            </div>
+                        </Box>
+                        {/* ===================================== */}
                         <div className="row order-products justify-content-between" style={{ marginBottom: '30px' }}>
                             <div className="col-lg-9 fix-padding cart-scroll">
                                 {order.orderItems.length === 0 ? (
