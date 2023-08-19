a1a64ad90f8e9e1e2ba768dcce298d14dc05e553
HoTai1601 <hotai1601@gmail.com>
2023-08-19 21:16:57 +0700

    fix-2

--- admin/src/components/Home/TopTotal.js
+++ admin/src/components/Home/TopTotal.js
@@ -9,13 +9,13 @@ const TopTotal = (props) => {
     return (
         <div className="row">
             <div className="col-lg-3">
-                <div className="card card-body mb-4 shadow-sm boder-dabost">
+                <div className="card card-body boder-dabost mb-4 shadow-sm">
                     <article className="icontext">
                         <div className="d-flex justify-content-between align-items-center">
                             <span className="icon icon-sm rounded-circle alert-primary">
                                 <i className="text-primary fas fa-usd-circle"></i>
                             </span>
-                            <h6 className="mb-1 fs-5">Tổng doanh số</h6>
+                            <h6 className="fs-5 mb-1">Doanh số</h6>
                         </div>
                     </article>
                     <div className="text ps-3">
@@ -26,47 +26,47 @@ const TopTotal = (props) => {
                 </div>
             </div>
             <div className="col-lg-3">
-                <div className="card card-body mb-4 shadow-sm boder-dabost">
+                <div className="card card-body boder-dabost mb-4 shadow-sm">
                     <article className="icontext">
                         <div className="d-flex justify-content-between align-items-center">
                             <span className="icon icon-sm rounded-circle alert-success">
                                 <i className="text-success fas fa-bags-shopping"></i>
                             </span>
-                            <h6 className="mb-1 fs-5">Tổng đơn hàng hoàn tất</h6>
+                            <h6 className="fs-5 mb-1">Đơn hàng hoàn tất</h6>
                         </div>
                     </article>
-                    <div className="text ps-3 fs-5 fw-semibold">
+                    <div className="text fs-5 fw-semibold ps-3">
                         {orders ? <span>{orders.length}</span> : <span>0</span>}
                     </div>
                 </div>
             </div>
             <div className="col-lg-3">
-                <div className="card card-body mb-4 shadow-sm boder-dabost">
+                <div className="card card-body boder-dabost mb-4 shadow-sm">
                     <article className="icontext">
                         <div className="d-flex justify-content-between align-items-center">
                             <span className="icon icon-sm rounded-circle alert-warning">
                                 <i className="text-warning fad fa-shopping-cart"></i>
                             </span>
 
-                            <h6 className="mb-1 fs-5">Tổng sản phẩm</h6>
+                            <h6 className="fs-5 mb-1">Sản phẩm</h6>
                         </div>
                     </article>
-                    <div className="text ps-3 fs-5 fw-semibold">
+                    <div className="text fs-5 fw-semibold ps-3">
                         {countProducts ? <span>{countProducts}</span> : <span>0</span>}
                     </div>
                 </div>
             </div>
             <div className="col-lg-3">
-                <div className="card card-body mb-4 shadow-sm boder-dabost">
+                <div className="card card-body boder-dabost mb-4 shadow-sm">
                     <article className="icontext">
                         <div className="d-flex justify-content-between align-items-center">
                             <span className="icon icon-sm rounded-circle alert-success">
                                 <i className="text-success fas fa-user-alt"></i>
                             </span>
-                            <h6 className="mb-1 fs-5">Tổng tài khoản</h6>
+                            <h6 className="fs-5 mb-1">Tài khoản</h6>
                         </div>
                     </article>
-                    <div className="text ps-3 fs-5 fw-semibold">
+                    <div className="text fs-5 fw-semibold ps-3">
                         {countUsers ? <span>{countUsers}</span> : <span>0</span>}
                     </div>
                 </div>
--- admin/src/layouts/layoutComponents/Header/Header.js
+++ admin/src/layouts/layoutComponents/Header/Header.js
@@ -12,15 +12,15 @@ const Header = () => {
     };
 
     return (
-        <header className="main-header navbar header-color">
+        <header className="main-header navbar bg-sky-800">
             <div className="col-search"></div>
             <div className="col-nav">
                 <button className="btn btn-icon btn-mobile me-auto" data-trigger="#offcanvas_aside">
                     <i className="md-28 fas fa-bars"></i>
                 </button>
-                <ul className="nav">
-                    <li className="dropdown nav-item">
-                        <Link className="dropdown-toggle" data-bs-toggle="dropdown" to="#">
+                <ul>
+                    <li className="dropdown nav-item ">
+                        <Link className="dropdown-toggle d-flex align-items-center" data-bs-toggle="dropdown" to="#">
                             <img
                                 className="img-xs"
                                 style={{
@@ -36,8 +36,8 @@ const Header = () => {
                             />
                         </Link>
                         <div className="dropdown-menu dropdown-menu-end">
-                            <Link onClick={logoutHandler} className="dropdown-item text-danger" to="#">
-                                Đăng xuất
+                            <Link onClick={logoutHandler} className="dropdown-item text-center" to="#">
+                                <span>Đăng xuất</span>
                             </Link>
                         </div>
                     </li>
