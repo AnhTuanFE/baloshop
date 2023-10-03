8f6e7958ba0c9bdb9912fc03c9c3e5d0dfa36829
AnhTuandgtl <tuandgtl7@gmail.com>
2023-10-03 11:13:11 +0700

    đổi defaultLayout new

--- user/src/layouts/defaultLayoutNew/DefaultLayout.js
+++ user/src/layouts/defaultLayoutNew/DefaultLayout.js
@@ -1,5 +1,5 @@
 import { memo } from 'react';
-import Footer from '../componentsLayout/footer';
+import Footer from '../componentsLayout/footer/Footer';
 import Header from '../componentsLayout/Header';
 function DefaulLayout({ children }) {
     return (
